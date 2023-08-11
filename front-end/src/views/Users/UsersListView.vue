6d0078da9fd424d36bdb7c730388e0baa5f1559f
Ramon Almeida <ramongoncalves76@gmail.com>
2023-08-10 22:23:42 -0300

    feat: update table format

--- front-end/src/views/Users/UsersListView.vue
+++ front-end/src/views/Users/UsersListView.vue
@@ -1,33 +1,58 @@
 <template>
   <div>
-    <table class="table-fixed text-sm">
-      <thead class="text-left text-gray-800">
-        <tr>
-          <th>
-            <checkbox-input-less-icon v-if="indexesSelected.size < users.length"
-              :is-checked-input="indexesSelected.size > 0" @input-changed="v => eventTableCheckbox(v)" />
+    <div class="w-1/2 flex flex-col items-center text-sm">
+      <table class="w-full table-fixed">
+        <thead class="text-left text-gray-600">
+          <tr class="border-b-2">
+            <th class="p-2">
+              <div class="mt-1">
+                <checkbox-input-less-icon v-if="indexesSelected.size < users.length"
+                  :is-checked-input="indexesSelected.size > 0" @input-changed="v => eventTableCheckbox(v)" />
 
-            <checkbox-input-square-icon v-else :is-checked-input="true" @input-changed="v => eventTableCheckbox(v)" />
-          </th>
+                <checkbox-input-square-icon v-else :is-checked-input="true" @input-changed="v => eventTableCheckbox(v)" />
+              </div>
+            </th>
+            <th class="p-2">
+              <div>USUÁRIO</div>
+            </th>
+            <th class="p-2">
+              <div>EMAIL</div>
+            </th>
+            <th class="p-2 text-center">
+              <div>ATIVO</div>
+            </th>
+          </tr>
+        </thead>
 
-          <th>USUÁRIO</th>
-          <th>E-MAIL</th>
-          <th>ATIVO</th>
-        </tr>
-      </thead>
+        <tbody>
+          <tr v-for="(user, idx) in users" :key="idx" class="border-b-2">
+            <td class="w-1/2 p-2">
+              <div class="mt-1">
+                <checkbox-input-square-icon :is-checked-input="indexesSelected.has(idx)"
+                  @input-changed="v => eventItemCheckbox(v, idx)" />
+              </div>
+            </td>
+            <td class="p-2">{{ user.username }}</td>
+            <td class="p-2">{{ user.email }}</td>
+            <td class="p-2 text-center">
+              <p>{{ user.isActive ? 'sim' : 'não' }}</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-      <tbody>
-        <tr v-for="(user, idx) in users" :key="idx">
-          <td>
-            <checkbox-input-square-icon :is-checked-input="indexesSelected.has(idx)"
-              @input-changed="v => eventItemCheckbox(v, idx)" />
-          </td>
-          <td>{{ user.username }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ user.isActive ? 'sim' : 'não' }}</td>
-        </tr>
-      </tbody>
-    </table>
+      <ul class="w-full flex justify-center border-b-2 border-gray-200">
+        <li>1</li>
+        <li>2</li>
+      </ul>
+
+      <ul class="mt-4 py-2 flex border border-gray-500 rounded-lg text-xs">
+        <li v-for="(p, k) in t" :key="k" class="px-2 py-1 border-r last:border-r-0 border-gray-400">
+          <a href="" class="p-2">{{ p }}</a>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
@@ -47,6 +72,7 @@ export default defineComponent({
   data() {
     return {
       indexesSelected: new Set() as Set<number>,
+      t: ['<< Anterior', '1', '2', '3', '4', 'Próxima >>'],
       users: [
         { username: '@test1', email: 'test1@mail.com', isActive: true },
         { username: '@test2', email: 'test2@mail.com', isActive: true },
