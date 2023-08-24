69291d745b27d1fae017c4dbd0653486a2dcd14a
Ramon Almeida <ramongoncalves76@gmail.com>
2023-08-23 23:36:49 -0300

    feat: refac boxes

---
+++ front-end/src/components/boxes/AppBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col py-8 px-6 text-gray-700 bg-white rounded-md shadow-md">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'AppBox'
+});
+</script>
---
+++ front-end/src/components/boxes/BoxWithCloseButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <app-box>
+    <div class="">
+      <p class="float-left font-semibold">{{ title }}</p>
+
+      <a class="float-right cursor-pointer" @click="$emit('closeBox')">
+        <font-awesome-icon class="py-1 px-[6px] text-red-600 bg-red-200 rounded-full hover:bg-red-300"
+          icon="fa-solid fa-xmark" />
+      </a>
+    </div>
+
+    <slot></slot>
+  </app-box>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AppBox from './AppBox.vue';
+
+
+export default defineComponent({
+  name: 'BoxWithCloseButton',
+
+  components: { AppBox },
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+});
+</script>
