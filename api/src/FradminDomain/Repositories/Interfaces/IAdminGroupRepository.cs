600270f57c8fa97540ab32db137c766401bacd91
Ramon Almeida <ramongoncalves76@gmail.com>
2023-09-02 10:32:52 -0300

    feat: add method desc

--- api/src/FradminDomain/Repositories/Interfaces/IAdminGroupRepository.cs
+++ api/src/FradminDomain/Repositories/Interfaces/IAdminGroupRepository.cs
@@ -4,7 +4,27 @@ namespace FradminDomain.Repositories.Interfaces;
 
 public interface IAdminGroupRepository
 {
+    /**
+     * Get AdminGroup by name.
+     * <exception cref="Exception"></exception>
+     * <exception cref="BaseException"></exception>
+     * <returns>AdminGroup found, or null if not.</returns>
+     */
     public Task<AdminGroup>? GetByName(string name);
 
+    /**
+     * Get all AdminGroup.
+     * <exception cref="Exception"></exception>
+     * <exception cref="BaseException"></exception>
+     * <returns>AdminGroup List.</returns>
+     */
+    public Task<List<AdminGroup>> GetAll();
+
+    /**
+     * Add a new AdminGroup.
+     * <exception cref="Exception"></exception>
+     * <exception cref="BaseException"></exception>
+     * <returns>AdminGroup added.</returns>
+     */
     public Task<AdminGroup> Insert(AdminGroup adminGroup);
 }
--- api/src/FradminDomain/UseCases/Interfaces/IAdminGroupUseCase.cs
+++ api/src/FradminDomain/UseCases/Interfaces/IAdminGroupUseCase.cs
@@ -1,14 +1,20 @@
 using FradminDomain.DTOs;
-using FradminDomain.Exceptions;
 
 namespace FradminDomain.UseCases.Interfaces;
 
 public interface IAdminGroupUseCase
 {
     /**
+     * Get all AdminGroup.
+     * <exception cref="Exception"></exception>.
+     * <returns>AdminGroup List.</returns>
+     */
+    public Task<List<AdminGroupFullDto>> GetAll();
+
+    /**
      * Create a new AdminGroup.
-     * <exception cref="BaseException"></exception>
-     * <returns>Group Created.</returns>
+     * <exception cref="Exception"></exception>.
+     * <returns>AdminGroup Created.</returns>
      */
     public Task<AdminGroupFullDto> Add(AdminGroupNewDto groupDto);
 }
