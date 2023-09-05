477f889594986579646a38e0828399a754258c24
Ramon Almeida <ramongoncalves76@gmail.com>
2023-09-05 12:44:20 -0300

    feat: add desc section

--- front-end/src/views/management/ManagementGroupPage.vue
+++ front-end/src/views/management/ManagementGroupPage.vue
@@ -16,8 +16,8 @@
         </div>
       </div>
 
-      <div>
-        <app-table class="mt-8" :pagination="{ actual: 1, max: 2 }">
+      <div class="mt-8 overflow-y-auto overflow-x-auto">
+        <app-table :pagination="{ actual: 1, max: 2 }">
           <template v-slot:head>
             <table-head-item-with-sort class="py-2 px-3">ID</table-head-item-with-sort>
             <table-head-item-with-sort class="py-2 px-3">Name</table-head-item-with-sort>
@@ -36,38 +36,77 @@
       </div>
     </app-box>
 
-    <app-box class="w-1/2 md:h-[560px] ml-6 overflow-y-auto overflow-x-auto">
-      <div class="">
-        <p class="font-semibold">Grupo: Grupo1</p>
-
-        <p class="mt-1 text-sm">Habilite/Desabilite as permissões deste grupo</p>
-      </div>
-
+    <app-box class="flex flex-col justify-between w-1/2 md:h-[560px] ml-6 overflow-y-auto overflow-x-auto">
       <div class="text-sm">
-        <div v-for="temp in 2" :key="temp" class="mt-4">
-          <div class="pt-4 border-t border-dashed border-gray-300">
-            <p class="inline font-semibold">Gerenciamento</p>
-            <span class="mt-0.5 float-right"><checkbox-with-square-icon :is-checked-input="false" /></span>
+        <div class="mb-4">
+          <div class="font-semibold">
+            <label for="">{{ $t('label.group') }}: </label>
+            <input class="focus:outline-none" type="text" value="Grupo 1">
+            <p class="inline-block float-right py-0.5 px-2 text-xs text-slate-50 bg-red-500 rounded-md">
+              Alterado: Grupo 1
+            </p>
           </div>
 
-          <table class="mt-3 table table-auto mx-auto">
-            <tbody>
-              <tr v-for="idx in Math.ceil(perm.length / tableWrap)" :key="idx" class="">
-                <td v-for="i in Math.min(perm.length - (idx - 1) * tableWrap, tableWrap)"
-                  :key="(idx - 1) * tableWrap + (i - 1)" class="py-2 pr-6">
-
-                  <span class="mt-0.5"><checkbox-with-square-icon :is-checked-input="false" /></span>
-                  <span class="ml-3"> {{ perm[(idx - 1) * tableWrap + (i - 1)] }}</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <p class="mt-1 text-sm">Configure permissões/usuários deste grupo.</p>
         </div>
 
-        <div class="mt-6">
-          <button-with-loading
-            :button-with-loading-desc="{ label: 'Salvar', icon: { icon: 'fa-solid fa-floppy-disk' } }" />
+        <div v-for="(ruleGroup, idx) in ruleGroups" :key="idx" class="pt-4 border-t border-dashed border-gray-300">
+          <div class="">
+            <p class="inline font-semibold">{{ $t(`label.${ruleGroup.name}`) }}</p>
+            <span class="mt-0.5 float-right">
+              <checkbox-with-square-icon :is-checked-input="adminGroupOpen.ruleGroupsAllSelected[idx]"
+                @inputChanged="v => inputRuleGroupChanged(ruleGroup.rules, v)" />
+            </span>
+          </div>
+
+          <ul class="grid grid-cols-4 py-3 px-4">
+            <li v-for="rule in ruleGroup.rules" :key="rule.id" class="py-2">
+              <span class="mt-0.5"><checkbox-with-square-icon :is-checked-input="adminGroupOpen.actualRule.has(rule.id)"
+                  @inputChanged="v => inputRuleChanged(rule.id, v)" /></span>
+              <span class="ml-3">{{ $t(`label.${rule.name}`) }}</span>
+            </li>
+          </ul>
         </div>
+
+        <div class="pt-4 mt-4 border-t border-dashed border-gray-300">
+          <p class="text-center font-semibold">Lista de Usuários</p>
+
+          <div class="mt-4 h-52 overflow-y-auto overflow-x-auto">
+            <app-table :pagination="{ actual: 1, max: 2 }">
+              <template v-slot:head>
+                <table-head-item-with-sort class="py-2 px-3">
+                  <checkbox-with-square-icon :is-checked-input="false" @inputChanged="v => { }" />
+                </table-head-item-with-sort>
+                <table-head-item-with-sort class="py-2 px-3">Usuário</table-head-item-with-sort>
+                <table-head-item-with-sort class="py-2 px-3">Status</table-head-item-with-sort>
+              </template>
+
+              <template v-slot:body>
+                <tr v-for="(group, idx) in tableData.getItems()" :key="idx"
+                  class="border-b cursor-pointer even:bg-white odd:bg-slate-100">
+                  <td class="p-3">
+                    <checkbox-with-square-icon :is-checked-input="false" @inputChanged="v => { }" />
+                  </td>
+                  <td class="p-3">@{{ group.name }}</td>
+                  <td class="p-3">
+                    <p v-if="group.qtUsers !== 50"
+                      class="inline-block py-0.5 px-2 text-xs text-slate-50 bg-green-600 rounded-md">
+                      Ativo
+                    </p>
+                    <p v-else class="inline-block py-0.5 px-2 text-xs text-slate-50 bg-red-500 rounded-md">
+                      Inativo
+                    </p>
+                  </td>
+                </tr>
+              </template>
+            </app-table>
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-8">
+        <button-with-loading
+          :button-with-loading-desc="{ label: 'Salvar', icon: { icon: 'fa-solid fa-floppy-disk' }, isLoading: adminGroupOpen.isChanged }" />
       </div>
     </app-box>
   </div>
@@ -84,10 +123,20 @@ import Table from '@/models/Table';
 import TableHeadItemWithSort from '@/components/table/TableHeadItemWithSort.vue';
 
 
-interface IGroup {
+interface IAdminGroup {
   id: number;
   name: string;
   qtUsers: number;
+}
+
+interface IRule {
+  id: number;
+  name: string
+}
+
+interface IRuleGroup {
+  name: string,
+  rules: IRule[]
 }
 
 
@@ -99,17 +148,18 @@ export default defineComponent({
   data() {
     return {
       menuStore: menuItemStore(),
-      tableData: new Table<IGroup>(),
-      tableWrap: 4,
-      perm: [
-        'Grupo 1',
-        'Grupo 2',
-        'Grupo 3',
-        'Grupo 4',
-        'Grupo 5',
-        'Grupo 6',
-        'Grupo 7'
-      ]
+      tableData: new Table<IAdminGroup>(),
+      ruleGroups: [
+        { name: 'management', rules: [{ id: 0, name: 'admin' }, { id: 1, name: 'group' }] },
+        { name: 'user', rules: [{ id: 2, name: 'list' }, { id: 3, name: 'add' }, { id: 4, name: 'list' }, { id: 5, name: 'list' }] },
+        { name: 'nas', rules: [{ id: 6, name: 'list' }, { id: 7, name: 'add' }, { id: 8, name: 'list' }, { id: 9, name: 'list' }] }
+      ],
+      adminGroupOpen: {
+        ruleGroupsAllSelected: [false, false, false],
+        beforeRules: new Set<number>(),
+        actualRule: new Set<number>(),
+        isChanged: false
+      }
     };
   },
 
@@ -120,7 +170,66 @@ export default defineComponent({
       { id: 1, name: 'Group 1', qtUsers: 15 },
       { id: 2, name: 'Group 2', qtUsers: 1 },
       { id: 3, name: 'Group 3', qtUsers: 50 },
+      { id: 1, name: 'Group 1', qtUsers: 15 },
+      { id: 2, name: 'Group 2', qtUsers: 1 },
+      { id: 3, name: 'Group 3', qtUsers: 50 },
+      { id: 1, name: 'Group 1', qtUsers: 15 },
+      { id: 2, name: 'Group 2', qtUsers: 1 },
+      { id: 3, name: 'Group 3', qtUsers: 50 },
+      { id: 1, name: 'Group 1', qtUsers: 15 },
+      { id: 2, name: 'Group 2', qtUsers: 1 },
+      { id: 3, name: 'Group 3', qtUsers: 50 },
     ]);
+  },
+
+  watch: {
+
+  },
+
+  methods: {
+    inputRuleChanged(id: number, isChecked: boolean): void {
+      var idExist = this.adminGroupOpen.actualRule.has(id);
+
+      if ((idExist && isChecked) || (!idExist && !isChecked)) return;
+
+      !idExist && isChecked ? this.adminGroupOpen.actualRule.add(id) : this.adminGroupOpen.actualRule.delete(id);
+
+      this.openRulesHaveChanged();
+      this.k();
+    },
+
+    inputRuleGroupChanged(rules: IRule[], isChecked: boolean): void {
+      for (const rule of rules) this.inputRuleChanged(rule.id, isChecked);
+
+      console.log(this.adminGroupOpen.isChanged)
+    },
+
+    k(): void {
+      for (var i = 0; i < this.ruleGroups.length; ++i) {
+        const ruleGroup = this.ruleGroups[i];
+
+        var idx = 0;
+
+        while (idx < ruleGroup.rules.length && this.adminGroupOpen.actualRule.has(ruleGroup.rules[idx].id)) ++idx;
+
+        this.adminGroupOpen.ruleGroupsAllSelected[i] = idx === ruleGroup.rules.length;
+      }
+    },
+
+    openRulesHaveChanged(): void {
+      if (this.adminGroupOpen.beforeRules.size !== this.adminGroupOpen.actualRule.size) {
+        this.adminGroupOpen.isChanged = true;
+        return;
+      };
+
+      for (const rule of this.adminGroupOpen.beforeRules.values())
+        if (!this.adminGroupOpen.actualRule.has(rule)) {
+          this.adminGroupOpen.isChanged = true;
+          return;
+        }
+
+      this.adminGroupOpen.isChanged = false;
+    }
   }
 });
 </script>
